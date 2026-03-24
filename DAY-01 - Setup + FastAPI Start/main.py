bcea72249cabbaeb05f169ac116cb6a0bafdbed0
Gurveer Singh <sgurveer97@gmail.com>
2026-03-24 21:56:47 +0530

    docs: Add inline comments to FastAPI setup and test route.

--- DAY-01 - Setup + FastAPI Start/main.py
+++ DAY-01 - Setup + FastAPI Start/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
-app = FastAPI()
+from fastapi import FastAPI #importing FastAPI
+app = FastAPI() #creating an instance of FastAPI
 
-# make Routes now
-@app.get("/test")
-def test():
-  return {"message":"Testingggg"}
+
+@app.get("/test") #decorator to define a route
+def test(): #defining a function to handle the GET request to /test
+  return {"message":"Testingggg"} #returning a JSON response
   
 @app.get("/hello")
 def hello():
